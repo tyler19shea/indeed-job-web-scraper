6cab26e7bac389d17619f48f7dc2c0b4f281b7fb
tyler19shea <170368134+tyler19shea@users.noreply.github.com>
2024-06-13 20:25:28 -0400

    Add the indeed-web-scraper.py program
    Adds the indeed web scraper that scrapes through a web page, specifically
    indeed. It turns its content including the title of the job, the company,
    the location, and summary. Content was added to README.md to give context
    to the program and its abilities and limitations. It also includes additions
    that can be implemented later.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # indeed-job-web-scraper
+This program utilizes the selenium library to scrape through a dynamic website. The website it scrapes is indeed and then places its contents into a csv file to organize it. 
+
+The libraries used are the BeautifulSoup library to handle the HTML content and then pandas library (along with the selenium library). The libraries can be installed by utilizing the folowing command:
+pip install pandas selenium beautifulsoup4
+
+The program fetches the web page using the chrome webdriver and returns the contents of the of page to be parsed using the BeautifulSoup library. The parsed data is then saved into a csv after placing the data into a pandas Data Frame. 
+
+I would like to add a more dynamic approach where the I can input the job title and location to search for. Another addition that would be benificial would be the ability to to get the other pages of the website so the list can be longer. 
---
+++ indeed-web-scraper.py
@@ -0,0 +1,57 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def fetch_web_page (url):
+    driver.get(url)
+    time.sleep(3)
+    return driver.page_source
+
+def parse_html(content):
+    soup = BeautifulSoup(content, "html.parser")
+    job_cards = soup.find_all('div', class_='slider_container')
+    return job_cards
+
+def extract_job_data(job_cards):
+    job_list = []
+
+    for item in job_cards:
+        title = item.find('h2', class_='jobTitle').text
+        company = item.find('span', class_='css-63koeb').get_text()
+        location = item.find('div', class_='css-1p0sjhy').get_text()
+        summary = item.find('div', class_='css-9446fg').get_text()
+        job_list.append({'Title': title, 'company': company, 'location': location, 'summary': summary})
+    return job_list
+
+def save_to_csv(job_list, filename='indeed_jobs.csv'):
+    df = pd.DataFrame(job_list)
+    df.to_csv(filename, index=False)
+    print(f'Data saved to {filename}')
+
+def main():
+    job_title = "Engineer"
+    location = "Richmond, va"
+    url = f"https://www.indeed.com/jobs?q={job_title}&l={location}"
+
+
+    content = fetch_web_page(url)
+    job_cards = parse_html(content)
+    jobs = extract_job_data(job_cards)
+
+    save_to_csv(jobs)
+
+
+if __name__ == '__main__':
+    try:
+        driver = webdriver.Chrome()
+        main()
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        driver.quit()
+
+
+
+
+
